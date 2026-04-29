19b6d3c634785d28e1cc77e9330e35390c1b2992
Vijay Hiremath <voidprotocol18@gmail.com>
2026-04-29 08:01:58 +0530

    Update db.ts
    connecDb to connectDB

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,7 +1,7 @@
 import * as mongoose from "mongoose";
 import {env} from "./env.ts";
 
-export const connecDB = async (): Promise<void> =>  {
+export const connectDB = async (): Promise<void> =>  {
      try {                 
         await mongoose.connect(env.MONGO_URI);
         console.log("MongoDb connected ! ✅");
