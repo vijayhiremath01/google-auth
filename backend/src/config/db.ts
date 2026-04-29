d57503befb8127dacc050c195af0514c17f6dbed
Vijay Hiremath <voidprotocol18@gmail.com>
2026-04-29 08:01:18 +0530

    Update db.ts
    made it better looking

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -2,10 +2,10 @@ import * as mongoose from "mongoose";
 import {env} from "./env.ts";
 
 export const connecDB = async (): Promise<void> =>  {
-          try {
-                  await mongoose.connect(env.MONGO_URI);
-                  console.log("MongoDb connected ! ✅");
-              } catch (e) {
-                  console.log("MongoDb not connected ❌")
-              }
+     try {                 
+        await mongoose.connect(env.MONGO_URI);
+        console.log("MongoDb connected ! ✅");
+     } catch (e) {
+         console.log("MongoDb not connected ❌")
+            }
 }
